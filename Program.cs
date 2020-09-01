8c927269676d1b5540447d34b4e55afc53255599
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2020-09-01 16:42:58 +0300

    Починил программу

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Homework
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
